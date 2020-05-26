fe60ead88609d709eaa5ca0344b2292d147f9d6b
Maksym1910 <55399296+Maksym1910@users.noreply.github.com>
2020-05-26 20:08:46 +0300

    realized Player and Square classes

--- app/src/main/java/com/example/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.tictactoeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TableLayout tableLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tableLayout = findViewById(R.id.activity_main);
+        //buildGameField();
     }
 }
---
+++ app/src/main/java/com/example/tictactoeapp/Player.java
@@ -0,0 +1,13 @@
+package com.example.tictactoeapp;
+
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public CharSequence getName(String name) {
+        return name;
+    }
+}
