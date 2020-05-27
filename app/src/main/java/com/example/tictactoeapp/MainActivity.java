657348d99340da1d16adc069d4cfc9a876663790
Maksym1910 <55399296+Maksym1910@users.noreply.github.com>
2020-05-27 12:23:24 +0300

    configured the layout

--- app/src/main/java/com/example/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/example/tictactoeapp/MainActivity.java
@@ -7,14 +7,9 @@ import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TableLayout tableLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tableLayout = findViewById(R.id.activity_main);
-        //buildGameField();
     }
 }
